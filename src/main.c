8ead53602307170f56504eadcf5629c17a8b457c
Albie Baker-Smith <bakersmitha@gmail.com>
2018-06-28 15:47:09 +0100

    Uses lexer

--- src/main.c
+++ src/main.c
@@ -2,72 +2,44 @@
 #include <stdlib.h>
 #include "lexer.h"
 
-// typedef char* Tokens;
-//
-// // Returns the next token on the input stream.
-// char lookahead(Tokens *ts) {
-//     return **ts;
-// }
-//
-// // Consumes the next token on the input stream of tokens.
-// void consume(Tokens *ts) {
-//     *ts += 1;
-// }
-//
-// // Matches on terms, i.e. digits 0-9.
-// void term(Tokens *ts) {
-//     char t = lookahead(ts);
-//     if (isdigit(t)) {
-//         printf("%c", t);
-//         consume(ts);
-//     } else {
-//         printf("\nExpected digit but got %c\n", t);
-//         exit(0);
-//     }
-// }
-//
-// // Matches on expressions, i.e. digits separated by operators. E.g. 0+1, 9+5+1
-// void expr(Tokens *ts) {
-//     term(ts);
-//     while (1) {
-//         char t = lookahead(ts);
-//
-//         if (t == '+') {
-//             consume(ts); term(ts); printf("+");
-//         }
-//         else if (t == '-') {
-//             consume(ts); term(ts); printf("-");
-//         }
-//         else {
-//             break;
-//         }
-//     }
-// }
-//
-// int main(int argc, char *argv[argc]) {
-//     if (argc != 2) {
-//         printf("Expected an expression to parse\n");
-//         return 0;
-//     }
-//
-//     Tokens *ts = &argv[1];
-//
-//     printf("%s\n", *ts);
-//     expr(ts);
-//     printf("\n");
-// }
+void err_expected(char *expected, Token actual) {
+    printf("Expected %s but got ", expected);
+    print_token(actual);
+    printf("\n");
+    exit(0);
+}
 
 void term(Tokens *input) {
     Token t = lookahead(input);
     switch (t.type) {
         case INT:
-            printf("%d", t.int_val);
+            printf("%d ", t.int_val);
+            consume(input);
             break;
         default:
-            printf("Expected integer, but got ");
-            print_token(t);
-            printf("\n");
+            err_expected("integer", t);
             break;
+    }
+}
+
+void expr(Tokens *input) {
+    term(input);
+    while (1) {
+        Token t = lookahead(input);
+
+        if (t.type == BIN_OP) {
+            consume(input);
+            term(input);
+            print_binop(t.bin_op);
+            printf(" ");
+        }
+        else if (t.type == DONE) {
+            break;
+        }
+        else {
+            err_expected("binary op", t);
+            break;
+        }
     }
 }
 
@@ -78,6 +50,6 @@ int main(int argc, char *argv[argc]) {
     }
 
     Tokens ts = from_string(argv[1]);
-    term(&ts);
+    expr(&ts);
     printf("\n");
 }

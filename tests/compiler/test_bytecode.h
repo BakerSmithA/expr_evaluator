b6bd529877328edc5595cd96dc9a74355e3391d0
Albie Baker-Smith <bakersmitha@gmail.com>
2018-07-02 09:55:55 +0100

    Tested bytecode equal

--- tests/compiler/test_bytecode.h
+++ tests/compiler/test_bytecode.h
@@ -31,19 +31,46 @@ bool test_bytecode_append_populated() {
 }
 
 // Tests two empty lists are equal.
-bool test_bytecode_eq() {
-    return false;
+bool test_bytecode_empty_eq() {
+    Bytecode *i1 = empty();
+    Bytecode *i2 = empty();
+    return eq_bytecode(i1, i2);
 }
 
 // Tests two popuated lists are equal.
+bool test_bytecode_populated_eq() {
+    Bytecode *i1 = empty();
+    append('a', i1);
+
+    Bytecode *i2 = empty();
+    append('a', i2);
+
+    return eq_bytecode(i1, i2);
+}
 
 // Tests two lists are not equal if their length is different.
+bool test_bytecode_len_not_eq() {
+    Bytecode *i1 = empty();
+    append('a', i1);
+    Bytecode *i2 = empty();
+    return !eq_bytecode(i1, i2);
+}
 
 // Tests two lists are different if the contain different instructions.
+bool test_bytecode_contents_not_eq() {
+    Bytecode *i1 = empty();
+    append('a', i1);
+    Bytecode *i2 = empty();
+    append('b', i2);
+    return !eq_bytecode(i1, i2);
+}
 
 // Runs all bytecode tests.
 void test_bytecode(Ctx ctx) {
     Ctx bytecode_ctx = nested_ctx("bytecode", ctx);
     test(test_bytecode_append_empty(), "append empty", bytecode_ctx);
     test(test_bytecode_append_populated(), "append populated", bytecode_ctx);
+    test(test_bytecode_empty_eq(), "empty eq", bytecode_ctx);
+    test(test_bytecode_populated_eq(), "populated eq", bytecode_ctx);
+    test(test_bytecode_contents_not_eq(), "contents not eq", bytecode_ctx);
 }
